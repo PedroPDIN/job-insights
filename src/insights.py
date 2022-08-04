6c452996d745d529fd652d88cdedd2aedfe2ff9b
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-04 20:23:16 -0300

    feat(requisito-6): adiciona requisito comleto.

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    all_jobs_industry = []
+    for job in jobs:
+        if job["industry"] == industry:
+            all_jobs_industry.append(job)
+    return all_jobs_industry
 
 
 def get_max_salary(path):
