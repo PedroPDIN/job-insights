a176cfbca2f8fcb7a643662762e6434c259f7637
Pedro Lima <pedro.oficial1969@gmail.com>
2022-09-29 17:47:01 -0300

    feat: adiciona novas alterações do projeto

--- tests/counter/test_counter.py
+++
@@ -1,7 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    assert count_ocurrences("src/jobs.csv", "Python") == 1639
-    assert count_ocurrences("src/jobs.csv", "JavaScript") == 122
-    assert count_ocurrences("src/jobs.csv", "Xablau") == 0
